2bd53ac3c9c52683b8fce7115744331cc4e4e3b5
Gayoso Facundo <facusbrr@gmail.com>
2024-06-27 16:02:03 -0300

    Se realizo el metodo POST

--- app.js
+++ app.js
@@ -9,10 +9,41 @@ const PORT = 3000;
 app.use(express.json());
 
 //Rutas
+
+//METODOS GET
 app.get('/products', (req, res) => {
     res.json(productos);
 });
 
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+    const producto = productos.find(producto => producto.id == id);
+    if (!producto) {
+        res.json({ error: 'El producto no se encuentra' });
+    }
+    res.json(producto);
+});
+
+//METODOS POST
+app.post("/products", (req, res) => {
+    const { name, quantity, price } = req.body;
+    const id = productos.length + 1; 
+    
+    if (!name || !quantity || !price) return res.status(400).send("Faltan datos");
+
+    const producto = {
+        id,
+        name,
+        quantity: parseInt(quantity), 
+        price: parseInt(price)
+    };
+    
+    productos.push(producto);
+    res.json({ message: "Producto agregado con éxito", producto });
+  });
+//METODOS PUT
+
+//METODOS DELETE
 
 //Poniendo en escucha el servidor
 app.listen(PORT, () => console.log(`Server funcionando en el puerto ${PORT}`))
