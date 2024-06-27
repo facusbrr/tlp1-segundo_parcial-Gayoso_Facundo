b2f26235ec52fc720bec0a7f34a99730c0bc849d
Gayoso Facundo <facusbrr@gmail.com>
2024-06-27 15:10:15 -0300

    Se creo el GET para poder devolver todos los productos

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const productos = require('./src/database/db.js');
 
 //Creando el server
 const app = express();
 const PORT = 3000;
 
+//Middlewares
+app.use(express.json());
+
+//Rutas
+app.get('/products', (req, res) => {
+    res.json(productos);
+});
 
 
 //Poniendo en escucha el servidor
---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+const productos = [
+  {
+    id: 1,
+    name: "Coca Cola",
+    quantity: "2",
+    price: "1900"
+  },
+  {
+    id: 2,
+    name: "Pepsi",
+    quantity: "4",
+    price: "1700"
+  },
+];
+
+module.exports = productos;
